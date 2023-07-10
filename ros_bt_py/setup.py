6a9bb8892c9f86da2fbcaaa8de11956293ca6b94
David Oberacker <oberacker@fzi.de>
2023-07-10 14:13:25 +0000

    Initial commit.
    Signed-off-by: David Oberacker <oberacker@fzi.de>

---
+++ ros_bt_py/setup.py
@@ -0,0 +1,25 @@
+# Copyright 2023 FZI Forschungszentrum Informatik
+
+from setuptools import setup
+
+package_name = "ros_bt_py"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="David Oberacker",
+    maintainer_email="oberacker@fzi.de",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [],
+    },
+)
