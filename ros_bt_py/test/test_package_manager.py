1562ccdabc29aa2bd7d3a74d3b07866d7f2578b5
David Oberacker <oberacker@fzi.de>
2023-07-19 15:48:55 +0200

    Remove test parameter that is ros2 version dependent.
    Signed-off-by: David Oberacker <oberacker@fzi.de>

--- ros_bt_py/test/test_package_manager.py
+++ ros_bt_py/test/test_package_manager.py
@@ -13,32 +13,6 @@ class TestPackageManager:
     @pytest.mark.parametrize(
         "msg_type, constant_values",
         [
-            (
-                "sensor_msgs/msg/BatteryState",
-                [
-                    "POWER_SUPPLY_STATUS_UNKNOWN",
-                    "POWER_SUPPLY_STATUS_CHARGING",
-                    "POWER_SUPPLY_STATUS_DISCHARGING",
-                    "POWER_SUPPLY_STATUS_NOT_CHARGING",
-                    "POWER_SUPPLY_STATUS_FULL",
-                    "POWER_SUPPLY_HEALTH_UNKNOWN",
-                    "POWER_SUPPLY_HEALTH_GOOD",
-                    "POWER_SUPPLY_HEALTH_OVERHEAT",
-                    "POWER_SUPPLY_HEALTH_DEAD",
-                    "POWER_SUPPLY_HEALTH_OVERVOLTAGE",
-                    "POWER_SUPPLY_HEALTH_UNSPEC_FAILURE",
-                    "POWER_SUPPLY_HEALTH_COLD",
-                    "POWER_SUPPLY_HEALTH_WATCHDOG_TIMER_EXPIRE",
-                    "POWER_SUPPLY_HEALTH_SAFETY_TIMER_EXPIRE",
-                    "POWER_SUPPLY_TECHNOLOGY_UNKNOWN",
-                    "POWER_SUPPLY_TECHNOLOGY_NIMH",
-                    "POWER_SUPPLY_TECHNOLOGY_LION",
-                    "POWER_SUPPLY_TECHNOLOGY_LIPO",
-                    "POWER_SUPPLY_TECHNOLOGY_LIFE",
-                    "POWER_SUPPLY_TECHNOLOGY_NICD",
-                    "POWER_SUPPLY_TECHNOLOGY_LIMN",
-                ],
-            ),
             ("geometry_msgs/msg/Twist", []),
             (
                 "ros_bt_py_interfaces/msg/Node",
