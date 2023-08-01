c43c288740d2b93d6253b3caaf9e44abe3ef5553
David Oberacker <oberacker@fzi.de>
2023-08-01 11:04:20 +0200

    Port RosParamInput and RosParamOptionDefaultInput
    Signed-off-by: David Oberacker <oberacker@fzi.de>

--- ros_bt_py/src/ros_bt_py/ros_nodes/param.py
+++ ros_bt_py/src/ros_bt_py/ros_nodes/param.py
@@ -9,7 +9,7 @@ from ros_bt_py.node_config import NodeConfig, OptionRef
 
 @define_bt_node(
     NodeConfig(
-        version="0.9.0",
+        version="0.1.0",
         options={
             "param_type": type,
             "default_value": OptionRef("param_type"),
@@ -32,6 +32,99 @@ class RosParamOption(Leaf):
             return NodeMsg.SUCCEEDED
         else:
             param = self.ros_node.get_parameter(self.options["param_name"])
+            param_value = param.value
+            if param_value is None:
+                self.outputs["param"] = self.options["default_value"]
+            else:
+                if isinstance(param, self.options["param_type"]):
+                    self.outputs["param"] = param_value
+                    return NodeMsg.SUCCEEDED
+                else:
+                    return NodeMsg.FAILED
+
+    def _do_shutdown(self):
+        pass
+
+    def _do_reset(self):
+        return NodeMsg.IDLE
+
+    def _do_untick(self):
+        return NodeMsg.IDLE
+
+    def _do_calculate_utility(self):
+        return UtilityBounds()
+
+
+@define_bt_node(
+    NodeConfig(
+        version="0.1.0",
+        options={
+            "param_type": type,
+            "param_name": str,
+        },
+        inputs={
+            "default_value": OptionRef("param_type"),
+        },
+        outputs={"param": OptionRef("param_type")},
+        max_children=0,
+    )
+)
+class RosParamOptionDefaultInput(Leaf):
+    """Read a parameter from the ROS parameter server."""
+
+    def _do_setup(self):
+        pass
+
+    def _do_tick(self):
+        if not self.ros_node.has_parameter(self.options["param_name"]):
+            self.outputs["param"] = self.inputs["default_value"]
+            return NodeMsg.SUCCEEDED
+        else:
+            param = self.ros_node.get_parameter(self.options["param_name"])
+            param_value = param.value
+            if param_value is None:
+                self.outputs["param"] = self.inputs["default_value"]
+            else:
+                if isinstance(param, self.options["param_type"]):
+                    self.outputs["param"] = param_value
+                    return NodeMsg.SUCCEEDED
+                else:
+                    return NodeMsg.FAILED
+
+    def _do_shutdown(self):
+        pass
+
+    def _do_reset(self):
+        return NodeMsg.IDLE
+
+    def _do_untick(self):
+        return NodeMsg.IDLE
+
+    def _do_calculate_utility(self):
+        return UtilityBounds()
+
+
+@define_bt_node(
+    NodeConfig(
+        version="0.1.0",
+        options={"param_type": type, "default_value": OptionRef("param_type")},
+        inputs={"param_name": str},
+        outputs={"param": OptionRef("param_type")},
+        max_children=0,
+    )
+)
+class RosParamInput(Leaf):
+    """Read a parameter from the ROS parameter server."""
+
+    def _do_setup(self):
+        pass
+
+    def _do_tick(self):
+        if not self.ros_node.has_parameter(self.inputs["param_name"]):
+            self.outputs["param"] = self.options["default_value"]
+            return NodeMsg.SUCCEEDED
+        else:
+            param = self.ros_node.get_parameter(self.inputs["param_name"])
             param_value = param.value
             if param_value is None:
                 self.outputs["param"] = self.options["default_value"]
