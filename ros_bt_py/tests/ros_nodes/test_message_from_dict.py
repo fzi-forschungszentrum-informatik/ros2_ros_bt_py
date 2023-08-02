aeebd590a2f06025b88fbdaf4c199b20fae70ffb
David Oberacker <oberacker@fzi.de>
2023-08-02 11:17:25 +0200

    Port message_from_dict nodes
    Signed-off-by: David Oberacker <oberacker@fzi.de>

---
+++ ros_bt_py/tests/ros_nodes/test_message_from_dict.py
@@ -0,0 +1,114 @@
+import pytest
+from ros_bt_py.ros_nodes.messages_from_dict import MessageFromDict
+from std_msgs.msg import Int64, String
+from std_srvs.srv import SetBool
+from ros_bt_py_interfaces.srv import FixYaml
+from ros_bt_py_interfaces.msg import Node as NodeMsg
+
+
+@pytest.mark.parametrize(
+    "message, message_dict",
+    [
+        (Int64, {"data": 667}),
+        (String, {"data": "this is a test string"}),
+        (SetBool.Request, {"data": True}),
+    ],
+)
+def test_node_success(message, message_dict):
+    node = MessageFromDict(options={"message_type": message})
+    node.setup()
+    assert node.state == NodeMsg.IDLE
+    node.inputs["dict"] = message_dict
+    node.tick()
+    out_message = node.outputs["message"]
+    assert out_message is not None
+    for attr, attr_value in message_dict.items():
+        assert getattr(out_message, attr) == attr_value
+
+    node.shutdown()
+    assert node.state == NodeMsg.SHUTDOWN
+
+
+@pytest.mark.parametrize(
+    "message, message_dict",
+    [
+        (Int64, {"data": 667}),
+        (String, {"data": "this is a test string"}),
+        (SetBool.Request, {"data": True}),
+    ],
+)
+def test_node_untick(message, message_dict):
+    node = MessageFromDict(options={"message_type": message})
+    node.setup()
+    assert node.state == NodeMsg.IDLE
+    node.inputs["dict"] = message_dict
+
+    node.tick()
+    assert node.state == NodeMsg.SUCCEED
+    out_message = node.outputs["message"]
+    assert out_message is not None
+    for attr, attr_value in message_dict.items():
+        assert getattr(out_message, attr) == attr_value
+
+    node.untick()
+    assert node.state == NodeMsg.IDLE
+
+    node.tick()
+    assert node.state == NodeMsg.SUCCEED
+    out_message = node.outputs["message"]
+    assert out_message is not None
+    for attr, attr_value in message_dict.items():
+        assert getattr(out_message, attr) == attr_value
+
+    node.shutdown()
+    assert node.state == NodeMsg.SHUTDOWN
+
+
+@pytest.mark.parametrize(
+    "message, message_dict",
+    [
+        (Int64, {"data": 667}),
+        (String, {"data": "this is a test string"}),
+        (SetBool.Request, {"data": True}),
+    ],
+)
+def test_node_reset(message, message_dict):
+    node = MessageFromDict(options={"message_type": message})
+    node.setup()
+    assert node.state == NodeMsg.IDLE
+    node.inputs["dict"] = message_dict
+
+    node.tick()
+    assert node.state == NodeMsg.SUCCEED
+    out_message = node.outputs["message"]
+    assert out_message is not None
+    for attr, attr_value in message_dict.items():
+        assert getattr(out_message, attr) == attr_value
+
+    node.reset()
+    assert node.state == NodeMsg.IDLE
+
+    node.tick()
+    assert node.state == NodeMsg.SUCCEED
+    out_message = node.outputs["message"]
+    assert out_message is not None
+    for attr, attr_value in message_dict.items():
+        assert getattr(out_message, attr) == attr_value
+
+    node.shutdown()
+    assert node.state == NodeMsg.SHUTDOWN
+
+
+@pytest.mark.parametrize(
+    "message, message_dict", [(FixYaml.Request, {"tequila": False})]
+)
+def test_node_failure(message, message_dict):
+    node = MessageFromDict(options={"message_type": message})
+    node.setup()
+    assert node.state == NodeMsg.IDLE
+    node.inputs["dict"] = message_dict
+    node.tick()
+    assert node.state == NodeMsg.FAILURE
+
+    out_message = node.outputs["message"]
+    assert out_message is None
