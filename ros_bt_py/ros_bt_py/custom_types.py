274a2be4ae9534db4a5e70fa0d83a9b02df15ba9
Doomse <hoefer@fzi.de>
2024-12-28 16:22:20 +0100

    Add Ros Service types

--- ros_bt_py/ros_bt_py/custom_types.py
+++ ros_bt_py/ros_bt_py/custom_types.py
@@ -26,28 +26,47 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+import rosidl_runtime_py
+import rosidl_runtime_py.utilities
+
 
 class FilePath(object):
-
     def __init__(self, path=''):
         self.path = path
 
 
 class MathUnaryOperator(object):
-    def __init__(self, operator):
+    def __init__(self, operator='sqrt'):
         self.operator = operator
 
 
 class MathBinaryOperator(object):
-    def __init__(self, operator):
+    def __init__(self, operator='+'):
         self.operator = operator
 
 
 class MathOperandType(object):
-    def __init__(self, operand_type):
+    def __init__(self, operand_type='float'):
         self.operand_type = operand_type
 
 
 class MathUnaryOperandType(object):
-    def __init__(self, operand_type):
+    def __init__(self, operand_type='float'):
         self.operand_type = operand_type
+
+
+class RosServiceType(object):
+    def __init__(self, type_str=''):
+        self.type_str = type_str
+
+    @property
+    def type_obj(self):
+        #NOTE We can't do the conversion in `__init__`,
+        # because jsonpickle bypasses the init function.
+        return rosidl_runtime_py.utilities.get_service(self.type_str)
+
+#NOTE This should only be used if the service name is specified via options.
+# If it is specified via inputs, this would involve unnecessary object creation
+class RosServiceName(object):
+    def __init__(self, name=''):
+        self.name = name
