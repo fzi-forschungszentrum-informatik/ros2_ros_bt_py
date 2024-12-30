649b0a0412001e4f66fb48d4715005bc92444dd3
Doomse <hoefer@fzi.de>
2024-12-30 15:30:22 +0100

    Implement Ros Topic types

--- ros_bt_py/ros_bt_py/ros_nodes/messages_from_dict.py
+++ ros_bt_py/ros_bt_py/ros_nodes/messages_from_dict.py
@@ -26,31 +26,70 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 """BT nodes to convert Dicts to ROS messages of a specific type."""
-from ros_bt_py_interfaces.msg import Node as NodeMsg
+
+from typing import Dict, Optional
+
+from rclpy.node import Node
 from rosidl_runtime_py.set_message import set_message_fields
 
 from ros_bt_py.node import Leaf, define_bt_node
 from ros_bt_py.node_config import NodeConfig, OptionRef
+from ros_bt_py.custom_types import RosTopicType
+from ros_bt_py.debug_manager import DebugManager
+from ros_bt_py.subtree_manager import SubtreeManager
+
+from ros_bt_py_interfaces.msg import Node as NodeMsg
+
 
 
 @define_bt_node(
     NodeConfig(
         version="0.1.0",
-        options={"message_type": type},
+        options={"message_type": RosTopicType},
         inputs={"dict": dict},
-        outputs={"message": OptionRef("message_type")},
+        outputs={},
         max_children=0,
     )
 )
 class MessageFromDict(Leaf):
     """Fill a ROS message with the values from `dict`."""
 
+    def __init__(
+        self,
+        options: Optional[Dict] = None,
+        debug_manager: Optional[DebugManager] = None,
+        subtree_manager: Optional[SubtreeManager] = None,
+        name: Optional[str] = None,
+        ros_node: Optional[Node] = None,
+        succeed_always: bool = False,
+        simulate_tick: bool = False,
+    ):
+        super(MessageFromDict, self).__init__(
+            options=options,
+            debug_manager=debug_manager,
+            subtree_manager=subtree_manager,
+            name=name,
+            ros_node=ros_node,
+            succeed_always=succeed_always,
+            simulate_tick=simulate_tick,
+        )
+
+        self._message_type = self.options["message_type"].get_type_obj()
+
+        node_outputs = {"message": self._message_type}
+
+        self.node_config.extend(
+            NodeConfig(options={}, inputs={}, outputs=node_outputs, max_children=0)
+        )
+
+        self._register_node_data(source_map=node_outputs, target_map=self.outputs)
+
     def _do_setup(self):
         pass
 
     def _do_tick(self):
         if self.inputs.is_updated("dict") or self.state == NodeMsg.IDLE:
-            message = self.options["message_type"]()
+            message = self._message_type()
             try:
                 set_message_fields(
                     message,
@@ -62,7 +101,7 @@ class MessageFromDict(Leaf):
                 TypeError,
             ) as ex:
                 self.logerr(
-                    f"Error populating message of type {self.options['message_type'].__name__}: "
+                    f"Error populating message of type {self._message_type.__name__}: "
                     f"{str(ex)}"
                 )
                 return NodeMsg.FAILED
@@ -81,20 +120,50 @@ class MessageFromDict(Leaf):
 @define_bt_node(
     NodeConfig(
         version="0.9.0",
-        options={"message_type": type, "dict": dict},
+        options={"message_type": RosTopicType, "dict": dict},
         inputs={},
-        outputs={"message": OptionRef("message_type")},
+        outputs={},
         max_children=0,
     )
 )
 class MessageFromConstDict(Leaf):
     """Fill a ROS message with the values from `dict`."""
 
+    def __init__(
+        self,
+        options: Optional[Dict] = None,
+        debug_manager: Optional[DebugManager] = None,
+        subtree_manager: Optional[SubtreeManager] = None,
+        name: Optional[str] = None,
+        ros_node: Optional[Node] = None,
+        succeed_always: bool = False,
+        simulate_tick: bool = False,
+    ):
+        super(MessageFromConstDict, self).__init__(
+            options=options,
+            debug_manager=debug_manager,
+            subtree_manager=subtree_manager,
+            name=name,
+            ros_node=ros_node,
+            succeed_always=succeed_always,
+            simulate_tick=simulate_tick,
+        )
+
+        self._message_type = self.options["message_type"].get_type_obj()
+
+        node_outputs = {"message": self._message_type}
+
+        self.node_config.extend(
+            NodeConfig(options={}, inputs={}, outputs=node_outputs, max_children=0)
+        )
+
+        self._register_node_data(source_map=node_outputs, target_map=self.outputs)
+
     def _do_setup(self):
         pass
 
     def _do_tick(self):
-        message = self.options["message_type"]()
+        message = self._message_type()
         try:
             set_message_fields(
                 message,
@@ -103,7 +172,7 @@ class MessageFromConstDict(Leaf):
             self.outputs["message"] = message
         except (TypeError, AttributeError) as ex:
             self.logerr(
-                f"Error populating message of type {self.options['message_type'].__name__}: "
+                f"Error populating message of type {self._message_type.__name__}: "
                 f"{str(ex)}"
             )
             return NodeMsg.FAILED
