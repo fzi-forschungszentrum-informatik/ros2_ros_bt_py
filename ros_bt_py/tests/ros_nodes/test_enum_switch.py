8be34bdcaee86ddda3fd516e167bb0a3fe9069bd
Niklas Spielbauer <spielbauer@fzi.de>
2025-06-10 15:53:26 +0000

    Added init tests
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>

---
+++ ros_bt_py/tests/ros_nodes/test_enum_switch.py
@@ -0,0 +1,64 @@
+# Copyright 2025 FZI Forschungszentrum Informatik
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the FZI Forschungszentrum Informatik nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+import pytest
+
+from ros_bt_py.exceptions import BehaviorTreeException
+from ros_bt_py.custom_types import RosTopicType
+from ros_bt_py_interfaces.msg import NodeState
+from ros_bt_py.ros_nodes.enum_switch import EnumSwitch
+
+
+@pytest.mark.parametrize(
+    "message",
+    [
+        RosTopicType("example_interfaces/msg/Empty"),
+    ],
+)
+def test_node_creation_failure(message):
+    with pytest.raises(ValueError):
+        EnumSwitch(
+            options={
+                "ros_message_type": message,
+            }
+        )
+
+
+@pytest.mark.parametrize(
+    "message",
+    [
+        RosTopicType("ros_bt_py_interfaces/msg/NodeState"),
+    ],
+)
+def test_node_creation_success(message):
+    enum_node = EnumSwitch(
+        options={
+            "ros_message_type": message,
+        }
+    )
+    enum_node.setup()
+    assert enum_node.state == NodeState.IDLE
