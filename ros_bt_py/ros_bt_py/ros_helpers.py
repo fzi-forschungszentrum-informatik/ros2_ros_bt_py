21c6dd616e952690e21408fe854ded84b4063817
Doomse <Hoefer@fzi.de>
2025-09-20 12:26:03 +0200

    Add uuid to message definitions

--- ros_bt_py/ros_bt_py/ros_helpers.py
+++ ros_bt_py/ros_bt_py/ros_helpers.py
@@ -27,10 +27,13 @@
 # POSSIBILITY OF SUCH DAMAGE.
 import inspect
 import array
+import uuid
 
 import rclpy.logging
 from rclpy import action
 from rclpy.node import Node, Publisher
+
+from unique_identifier_msgs.msg import UUID as ROS_UUID
 
 from ros_bt_py.exceptions import BehaviorTreeException
 from ros_bt_py_interfaces.msg import MessageChannel, MessageChannels
@@ -50,6 +53,16 @@ class EnumValue(object):
     def __init__(self, enum_value=""):
         """Initialize a new EnumValue class."""
         self.enum_value = enum_value
+
+
+def ros_to_uuid(ros_uuid_msg):
+    return uuid.UUID(bytes=bytes(ros_uuid_msg.uuid))
+
+
+def uuid_to_ros(uuid):
+    ros_uuid = ROS_UUID()
+    ros_uuid.uuid = list(uuid.bytes)  # Convert UUID bytes back to list
+    return ros_uuid
 
 
 def get_interface_name(msg_metaclass: type) -> str:
