1efc330e603f4a63dff5a63577af038aa712b9ad
Niklas Spielbauer <50439565+nspielbau@users.noreply.github.com>
2026-02-10 09:32:36 +0100

    Add Enum Switch Node (#183)
    * Added first version of node
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Swapped NodeMsg for NodeState
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Switched init behavior to not throw an exception
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Raise ValueError to give user feedback
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Catch ValueError to avoid crash
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Now reacts to Enum Type
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Added init tests
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Update to 0.5.x
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Update returns
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Move functionality to setup
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Update tests
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Remove orig file
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Fix Exception catch
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Fix return statements
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Fixup init method and check if input is there
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Add tests for EnumSwitch
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Fix tests
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    * Extended tests to explicitly check node states
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>
    
    ---------
    
    Signed-off-by: Niklas Spielbauer <spielbauer@fzi.de>

---
+++ ros_bt_py/ros_bt_py/ros_nodes/enum_switch.py
@@ -0,0 +1,191 @@
+# Copyright 2025 FZI Forschungszentrum Informatik
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the FZI Forschungszentrum Informatik nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from typing import Optional, Dict
+from rclpy.node import Node
+
+from result import Result, Ok, Err, is_err
+import uuid
+
+from ros_bt_py.debug_manager import DebugManager
+from ros_bt_py.subtree_manager import SubtreeManager
+from ros_bt_py.helpers import BTNodeState
+from ros_bt_py.exceptions import BehaviorTreeException, NodeConfigError
+
+from ros_bt_py.node import FlowControl, define_bt_node
+from ros_bt_py.node_config import NodeConfig
+
+from ros_bt_py.custom_types import TypeWrapper, RosTopicType, ROS_TYPE_FULL
+from ros_bt_py.ros_helpers import EnumValue, get_message_constant_fields
+
+
+@define_bt_node(
+    NodeConfig(
+        version="0.1.0",
+        options={"ros_message_type": TypeWrapper(RosTopicType, info=ROS_TYPE_FULL)},
+        inputs={},
+        outputs={},
+        max_children=None,
+    )
+)
+class EnumSwitch(FlowControl):
+    def __init__(
+        self,
+        node_id: Optional[uuid.UUID] = None,
+        options: Optional[Dict] = None,
+        debug_manager: Optional[DebugManager] = None,
+        subtree_manager: Optional[SubtreeManager] = None,
+        name: Optional[str] = None,
+        ros_node: Optional[Node] = None,
+    ) -> None:
+        super().__init__(
+            node_id=node_id,
+            options=options,
+            debug_manager=debug_manager,
+            subtree_manager=subtree_manager,
+            name=name,
+            ros_node=ros_node,
+        )
+        # Setup Child Dict
+        self._message_class = self.options["ros_message_type"].get_type_obj()
+
+        self.possible_children = get_message_constant_fields(self._message_class)
+
+        # Raise Error if no possible children
+        if not self.possible_children:
+            self.logerr(f"{self._message_class} has no constant fields")
+            return
+
+        # Check if all constants have equal type and raise error if not. Otherwise set input to
+        # constant type
+        pchild_types = [
+            type(getattr(self._message_class, field))
+            for field in self.possible_children
+        ]
+        if len(set(pchild_types)) > 1:
+            self.logerr(
+                f"{self._message_class} contains constant fields of multiple types"
+            )
+            return
+
+        node_inputs = {"case": pchild_types[0]}
+
+        extend_result = self.node_config.extend(
+            NodeConfig(options={}, inputs=node_inputs, outputs={}, max_children=None)
+        )
+        if extend_result.is_err():
+            raise extend_result.unwrap_err()
+
+        register_result = self._register_node_data(
+            source_map=node_inputs, target_map=self.inputs
+        )
+
+        if register_result.is_err():
+            raise register_result.unwrap_err()
+
+    def _do_setup(self) -> Result[BTNodeState, BehaviorTreeException]:
+        # Create possible child dict
+
+        self.msg = self._message_class()
+
+        self.pchild_dict = dict(
+            zip(
+                [getattr(self.msg, entry) for entry in self.possible_children],
+                self.possible_children,
+            )
+        )
+
+        self.child_map = {child.name.split(".")[-1]: child for child in self.children}
+        for child in self.children:
+            if child.name.split(".")[-1] not in self.possible_children:
+                self.logerr("Unwanted child detected please fix name")
+            match child.setup():
+                case Err(e):
+                    return Err(e)
+                case Ok(_):
+                    pass
+        return Ok(BTNodeState.IDLE)
+
+    def _do_tick(self) -> Result[BTNodeState, BehaviorTreeException]:
+        if "case" not in self.inputs:
+            self.logerr("Invalid message type")
+            return Ok(BTNodeState.FAILED)
+
+        name = self.inputs["case"]
+
+        if name in self.pchild_dict.keys():
+            e_name = self.pchild_dict[name]
+        else:
+            self.logwarn("Input did not match possible children.")
+            return Ok(BTNodeState.FAILED)
+
+        if e_name not in self.child_map:
+            self.logwarn(
+                "Ticking without matching child. Is this really what you want?"
+            )
+            return Ok(BTNodeState.FAILED)
+
+        # If we've previously succeeded or failed, untick all children
+        if self.state in [BTNodeState.SUCCEEDED, BTNodeState.FAILED]:
+            for child in self.children:
+                match child.reset():
+                    case Err(e):
+                        return Err(e)
+                    case Ok(_):
+                        pass
+
+        for child_name, child in self.child_map.items():
+            if not child_name == e_name:
+                match child.untick():
+                    case Err(e):
+                        return Err(e)
+                    case Ok(_):
+                        pass
+
+        return self.child_map[e_name].tick()
+
+    def _do_untick(self) -> Result[BTNodeState, BehaviorTreeException]:
+        for child in self.children:
+            match child.untick():
+                case Err(e):
+                    return Err(e)
+                case Ok(_):
+                    pass
+        return Ok(BTNodeState.IDLE)
+
+    def _do_reset(self) -> Result[BTNodeState, BehaviorTreeException]:
+        for child in self.children:
+            match child.reset():
+                case Err(e):
+                    return Err(e)
+                case Ok(_):
+                    pass
+        return Ok(BTNodeState.IDLE)
+
+    def _do_shutdown(self) -> Result[BTNodeState, BehaviorTreeException]:
+        return Ok(BTNodeState.SHUTDOWN)
---
+++ ros_bt_py/tests/integration/test_enum_switch.py
@@ -0,0 +1,134 @@
+# Copyright 2025 FZI Forschungszentrum Informatik
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the FZI Forschungszentrum Informatik nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+import pytest
+
+import rclpy
+
+import ament_index_python
+import os
+import time
+
+from rclpy.node import Node
+from rclpy.client import Client
+
+from ros_bt_py_interfaces.msg import (
+    NodeState,
+    TreeState,
+    TreeStateList,
+    TreeStructureList,
+)
+from ros_bt_py_interfaces.srv import (
+    ControlTreeExecution,
+    LoadTreeFromPath,
+)
+
+from conftest import launch_description
+
+
+@pytest.mark.parametrize(
+    "tree_name, result_dict",
+    [
+        (
+            "enum_switch_test_broken.yaml",
+            {
+                "MemorySequence": "FAILED",
+                "EnumSwitch": "FAILED",
+                "FOO": "IDLE",
+                "BAR": "IDLE",
+            },
+        ),
+        (
+            "enum_switch_test_fail.yaml",
+            {
+                "MemorySequence": "FAILED",
+                "EnumSwitch": "FAILED",
+                "FOO": "IDLE",
+                "BAR": "IDLE",
+            },
+        ),
+        (
+            "enum_switch_test_success.yaml",
+            {
+                "MemorySequence": "SUCCEEDED",
+                "EnumSwitch": "SUCCEEDED",
+                "FOO": "IDLE",
+                "BAR": "SUCCEEDED",
+            },
+        ),
+    ],
+)
+@pytest.mark.launch(fixture=launch_description)
+def test_enum_switch(
+    node: Node,
+    load_client: Client,
+    execute_client: Client,
+    state_list: list[TreeStateList],
+    structure_list: list[TreeStructureList],
+    result_dict: dict[str, str],
+    tree_name: str,
+):
+    tree_path = os.path.join(
+        ament_index_python.get_package_share_directory("ros_bt_py"),
+        "trees",
+        tree_name,
+    )
+    load_req = LoadTreeFromPath.Request(
+        path=f"file://{tree_path}",
+        permissive=False,
+    )
+    assert load_client.wait_for_service(timeout_sec=30)
+    load_future = load_client.call_async(load_req)
+    rclpy.spin_until_future_complete(node, load_future, timeout_sec=30)
+    assert load_future.done()
+    assert load_future.result().success
+
+    run_req = ControlTreeExecution.Request(
+        command=ControlTreeExecution.Request.TICK_UNTIL_RESULT,
+        tick_frequency_hz=10.0,
+    )
+    assert execute_client.wait_for_service(timeout_sec=30)
+    run_future = execute_client.call_async(run_req)
+    rclpy.spin_until_future_complete(node, run_future, timeout_sec=30)
+    assert run_future.done()
+    assert run_future.result().success
+
+    # Check in a loop if tree is in state IDLE (has run and completed)
+    start_time = time.time()
+    while start_time + 60 > time.time():
+        if state_list[-1].tree_states[0].state == TreeState.IDLE:
+            break
+        rclpy.spin_once(node, timeout_sec=10)
+    final_state_msg = state_list[-1].tree_states[0]
+    assert final_state_msg.state == TreeState.IDLE
+    nodes = structure_list[0].tree_structures[0].nodes
+    node_dict: dict[str, str] = {}
+    for node in nodes:
+        node_dict[node.node_id] = node.name
+    for node_state in final_state_msg.node_states:
+        if node_dict[node_state.node_id] in result_dict:
+            assert node_state.state == result_dict[node_dict[node_state.node_id]]
